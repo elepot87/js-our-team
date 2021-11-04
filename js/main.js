d6b40d4cbc0a6591a76250bac5055dfe1d2a0f45
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-04 15:31:45 +0100

    create array of objects with team members

--- js/main.js
+++ js/main.js
@@ -5,3 +5,49 @@
 // Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
 
 // 1. Creo un array di oggetti con i membri del team in mio possesso. Ogni oggetto avrà tre chiavi: immagine, nome e didascalia
+
+const teamMembers = [
+  // Member 1
+  {
+    img: "../img/wayne-barnett-founder-ceo.jpg",
+    firstLastName: "Wayne Barnett",
+    description: "Founder & CEO",
+  },
+  //   Member 2
+  {
+    img: "../img/angela-caroll-chief-editor.jpg",
+    firstLastName: "Angela Caroll",
+    description: "Chief Editor",
+  },
+  //   Member 3
+  {
+    img: "../img/walter-gordon-office-manager.jpg",
+    firstLastName: "Walter Gordon",
+    description: "Office Manager",
+  },
+  //   Member 4
+  {
+    img: "../img/angela-lopez-social-media-manager.jpg",
+    firstLastName: "Angela Lopez",
+    description: "Social media manager",
+  },
+  //   Member 5
+  {
+    img: "../img/scott-estrada-developer.jpg",
+    firstLastName: "Scott Estrada",
+    description: "Developer",
+  },
+  //   Member 6
+  {
+    img: "../img/barbara-ramos-graphic-designer.jpg",
+    firstLastName: "Barbara Ramos",
+    description: "Graphic Designer",
+  },
+];
+
+// // Vedere tutti gli oggetti dell'array
+// for (let i = 0; i < teamMembers.length; i++) {
+//   console.log(teamMembers[i].img);
+//   console.log(teamMembers[i].firstLastName);
+//   console.log(teamMembers[i].description);
+// }
