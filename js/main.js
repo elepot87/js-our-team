149e16b7f14036bf3f43831c14187658aa5ec92b
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-04 15:11:37 +0100

    first step of js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Ricreiamo le card del nostro team aggiungendo al layout di base fornito (allegato sotto) il nostro JavaScript in cui:
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+// Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
+
+// 1. Creo un array di oggetti con i membri del team in mio possesso. Ogni oggetto avrà tre chiavi: immagine, nome e didascalia
